45ec5bb9621e81fe67e9759420aef014c8e9a677
Brennan Predmore <brennnann@gmail.com>
2020-08-12 20:24:31 -0500

    init direction variables

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Create variable for inputdata === data
+// implement file reader from node
 // Convert it to a string
 // Create an array for each direction (N,E,S,W)
 // Map/loop through data and perform split ( , )
@@ -12,3 +13,10 @@ const input = fs.readFileSync(`${__dirname}/input-1.txt`).toString();
 const data = input.split(', ');
 const actionRegex =  /^(L|R)([0-9]+)$/;
 
+
+const DIRECTIONS = {
+    NORTH: 0,
+    EAST: 1,
+    SOUTH: 2,
+    WEST: 3
+};
